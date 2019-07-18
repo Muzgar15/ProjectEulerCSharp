61c2e48584cd2e40692da5514d26ac72fb109f51
Ambrož Tičar <ambroz.ticar@outlook.com>
2019-07-18 12:26:55 +0200

    Solved problem 9

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem9
+    {
+        public static int Solve()
+        {
+            for(int i = 1; i <= 1000; i++)
+            {
+                for(int j = 1; j <= 1000; j++)
+                {
+                    for(int k = 1; k <= 1000; k++)
+                    {
+                        if(i+j+k == 1000)
+                        {
+                            if (Math.Pow(i, 2) + Math.Pow(j, 2) == Math.Pow(k, 2))
+                            {
+                                return i * j * k;
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Problem8.Solve());
+            Console.WriteLine(Problem9.Solve());
             Console.ReadLine();
         }
     }
