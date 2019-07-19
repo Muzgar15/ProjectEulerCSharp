856b5af56fad089ea0cc8e72b0a8072b4c053436
Ambrož Tičar <ambroz.ticar@outlook.com>
2019-07-19 15:13:28 +0200

    Solved problem 14

---
+++ ProjectEuler/Problem14.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem14
+    {
+        public static long Solve()
+        {
+            long num = 1;
+            int counter = 1;
+            long max = 1;
+            long maxNum = 0;
+            for (int i = 1; i < 1000000; i++)
+            {
+                counter = 1;
+                num = i;
+                while (num != 1)
+                {
+                    counter++;
+                    if (num % 2 == 0)
+                    {
+                        num /= 2;
+                    }
+                    else
+                    {
+                        num = 3 * num + 1;
+                    }
+                }
+                if(counter > max)
+                {
+                    max = counter;
+                    maxNum = i;
+                }
+            }
+            return maxNum;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Problem13.Solve());
+            Console.WriteLine(Problem14.Solve());
             Console.ReadLine();
         }
     }
