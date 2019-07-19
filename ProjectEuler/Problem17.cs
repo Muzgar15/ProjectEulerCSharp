416574c8e182c0bfa446e413950e48e82be51a68
Ambrož Tičar <ambroz.ticar@outlook.com>
2019-07-19 22:13:46 +0200

    Solved problem 17

---
+++ ProjectEuler/Problem17.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem17
+    {
+        public static int Solve()
+        {
+            String allNumbers = "";
+            String hundred = "hundred";
+            String thousand = "thousand";
+            String and = "and";
+
+            int temp = 0;
+            String[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+            for (int i = 1; i <= 1000; i++)
+            {
+                temp = i;
+                if (i == 1000)
+                {
+                    allNumbers += numbers[1] + thousand;
+                    temp = 0;
+                }
+                else if (i >= 100)
+                {
+                    allNumbers += numbers[i / 100];
+                    allNumbers += hundred;
+                    temp %= 100;
+                    if (temp != 0)
+                    {
+                        allNumbers += and;
+                    }
+                }
+                if (temp != 0)
+                {
+                    if (temp < 20)
+                    {
+                        allNumbers += numbers[temp];
+                    }
+                    else
+                    {
+                        allNumbers += numbers[(temp / 10) + 18];
+                        temp %= 10;
+                        if (temp != 0)
+                        {
+                            allNumbers += numbers[temp];
+                        }
+
+                    }
+                }
+                allNumbers += "\n";
+            }
+            return allNumbers.Length;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Problem16.Solve());
+            Console.WriteLine(Problem17.Solve());
             Console.ReadLine();
         }
     }
